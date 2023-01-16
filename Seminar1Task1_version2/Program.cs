0e31084eb871da47e4b95774c02d2bba693dd2ec
Vladimir <vpolyakovup122@gmail.com>
2023-01-17 02:15:19 +0500

    Семинар 1. Задача 0. Версия 2

---
+++ Seminar1Task1_version2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача №1
+//Напишите программу, которая на вход принимаетдва числа и проверяет, является ли первое число квадратом второго.
+//Например:
+//a = 25; b = 5 -> да
+//a = 2 b = 10 -> нет
+//a = 9; b = -3 -> да
+//a = -3 b = 9 -> нет
+
+//Cчитываем данные с консоли 1 число
+string? inputNum1 = Console.ReadLine();
+//Cчитываем данные с консоли 2 число
+string? inputNum2 = Console.ReadLine();
+
+if ((inputNum1 != null) && (inputNum2 != null))
+{
+  if ((int)Math.Pow(int.Parse(inputNum2), 2) == int.Parse(inputNum1))
+  {
+    Console.Write("yes");
+  }
+  else
+  {
+    Console.Write("no");
+  }
+}
