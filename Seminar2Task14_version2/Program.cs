a84ea4aa09e33a5931dd5b5ef0b05a8965bd06e4
Vladimir <vpolyakovup122@gmail.com>
2023-01-18 02:10:14 +0500

    Семинар 2. Задача 14. Версия 2

---
+++ Seminar2Task14_version2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+//Вариант 2
+
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+int res1 = num1%7;
+int res2 = num1%23;
+if((res1==0)&&(res2==0))
+{
+Console.WriteLine("Кратное ");
+}
+
+else
+{
+Console.WriteLine("Не кратно " + res1 + " " + res2);
+}
