b42e5ed25142c824d40aac1d26b823f5f0badc09
Vladimir <vpolyakovup122@gmail.com>
2023-01-17 23:16:24 +0500

    Семинар 2. Задача 11.Нужно допилить

---
+++ Seminar2Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+//Вариант 2
+System.Random numSintezator = new System.Random();
+int num = numSintezator.Next(100, 1000);
+int digit1 = num/100;
+int digit2 = num%10;
+Console.WriteLine(digit1*10+digit2);
