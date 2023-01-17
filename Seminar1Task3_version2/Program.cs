561838f3cbc2adca9721a033d5036ba7201e2ac9
Vladimir <vpolyakovup122@gmail.com>
2023-01-18 00:30:57 +0500

    Семинар 1. Задача 3. Версия 2

---
+++ Seminar1Task3_version2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача №3
+//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//Например: 
+//3 -> Среда
+//5 -> Пятница
+
+//Считываем данные с консоли 
+string? imputLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if (imputLine != null)
+{
+//Вариант 2
+int imputNumber = int.Parse(imputLine);
+string outDayOfWeek = string.Empty;
+switch (imputNumber)
+{
+case 1: outDayOfWeek = "понедельник"; break;
+case 2: outDayOfWeek = "вторник"; break;
+case 3: outDayOfWeek = "среда"; break;
+case 4: outDayOfWeek = "четверг"; break;
+case 5: outDayOfWeek = "пятница"; break;
+case 6: outDayOfWeek = "суббота"; break;
+case 7: outDayOfWeek = "воскресенье"; break;
+default: outDayOfWeek = "это не день недели!"; break;
+}
+Console.WriteLine(outDayOfWeek);
+}
