40325e516580468c0f82d6e7f1ee6b4ee2606bf0
Vladimir <vpolyakovup122@gmail.com>
2023-01-18 00:43:18 +0500

    Семинар 1. Задача 3. Версия 3

--- Seminar1Task3_version3/Program.cs
+++ Seminar1Task3_version3/Program.cs
@@ -3,8 +3,6 @@ if (imputLine != null)
 {
 
     int imputNumber = int.Parse(imputLine);
- 
     string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(imputNumber));
     Console.WriteLine(outDayOfWeek);
-    
 }
