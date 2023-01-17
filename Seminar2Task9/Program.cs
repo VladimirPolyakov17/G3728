e5e50acee8a77bebe20bd07082cb19855afe168c
Vladimir <vpolyakovup122@gmail.com>
2023-01-17 23:03:59 +0500

    Семинар 2. Задача 9.Нужно допилить

---
+++ Seminar2Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа
+
+//Вариант 1
+System.Random numSintezator = new System.Random();
+
+//int rndNum = numSintezator.Next(10,100);
+//Console.WriteLine("Cлучайное число: " + rndNum);
+
+//int digit1 = rndNum/10;
+//int digit2 = rndNum%10;
+
+//if(digit1>digit2)
+//{
+// Console.WriteLine("Цифра 1 болььше цифры 2: "+ digit1 +" "+digit2);
+//}
+//else
+//{
+//     Console.WriteLine("Цифра 2 больше 1: " +digit1 + " " + digit2);
+// }
+
+// Варинант 2
+char [] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+int digit1 = (int)digits[0]-48;
+int digit2 = (int)digits[1]-48;
+int resNum = digit1>digit2?digit1:digit2;
+Console.WriteLine(resNum);
+
+//Вариант 3
