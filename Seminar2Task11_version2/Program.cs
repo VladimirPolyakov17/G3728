3c0657cb1bc7cb46aec766729958b618ed409d40
Vladimir <vpolyakovup122@gmail.com>
2023-01-18 01:43:13 +0500

    Семинар 2. Задача 11. Версия 2

---
+++ Seminar2Task11_version2/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Random newSintezator = new System.Random();
+int num = newSintezator.Next(100,1000);
+
+System.Random numSintezator = new System.Random();
+int digit1 = num/100;
+int digit2 = num%10;
+Console.WriteLine(num);
+Console.WriteLine(digit1*10+digit2);
