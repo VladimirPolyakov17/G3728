d4de4c3bf1f809adfca3425c338b2f5c06552897
Vladimir <vpolyakovup122@gmail.com>
2023-01-19 02:53:41 +0500

    Семинар 1. Задача 4. Домашнее задание

---
+++ Seminar1Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("input yor nums: ");
+//Cчитываем данные с консоли 1 число
+string? inputNum1 = Console.ReadLine();
+//Cчитываем данные с консоли 2 число
+string? inputNum2 = Console.ReadLine();
+//Cчитываем данные с консоли 3 число
+string? inputNum3 = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(inputNum1!=null && inputNum2!=null && inputNum3!=null)
+
+{
+  //Парсим введенные числа
+  int number1 = int.Parse(inputNum1);
+  int number2 = int.Parse(inputNum2);
+  int number3 = int.Parse(inputNum3);
+
+  //Вводим новую переменную равную значению функции находящей максимальное число.
+  int max = Math.Max(number1, Math.Max(number2, number3));
+  
+  //Вывод на консоль максимальное число
+  Console.WriteLine("max=" +max);
+}
