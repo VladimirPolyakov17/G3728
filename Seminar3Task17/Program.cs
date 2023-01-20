492a75851e6cf32c137c5d9f9540336ef650615d
Vladimir <vpolyakovup122@gmail.com>
2023-01-20 22:52:12 +0500

    Семинар 3. Задача 17. Нужно допилить

---
+++ Seminar3Task17/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача №17
+//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+int coordX=ReadDate("Введите координату X");
+int coordY=ReadDate("Введите координату Y");
+PrintQuterTest(coordX,coordY);
+
+//Сверху решение, чтобы не было как ниже многоразовых повторений
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите координату X");
+// int coordx = int.Parse(Console.ReadLine()??"0");
+
+//Метод читает данные от пользователя
+int ReadDate(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+
+}
+// Метод определяет четверть по координатам точки
+void PrintQuterTest()
+{
+  if(coordX > 0 && coordY > 0) Console.WriteLine("Точка в четверти 1");
+  if(coordX > 0 && coordY < 0) Console.WriteLine("Точка в четверти 2");
+  if(coordX < 0 && coordY < 0) Console.WriteLine("Точка в четверти 3");
+  if(coordX < 0 && coordY > 0) Console.WriteLine("Точка в четверти 4");
+}
