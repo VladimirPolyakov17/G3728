c5752badf546a176361cfd29d5384aadfe9bd155
Vladimir <vpolyakovup122@gmail.com>
2023-01-20 22:27:28 +0500

    Семинар 1. Задача 13. Нужно допилитть

--- Seminar2Task13/Program.cs
+++ Seminar2Task13/Program.cs
@@ -1,10 +1,33 @@
-﻿char[]digit = Console.ReadLine().ToCharArray();
+﻿// //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.Write("Введите трёхзначное число: ");
+// int number = int.Parse(Console.ReadLine()??"0");
+// //"Если значение более ста, то продолжаем, иначе выводим сообщение об ошибке"
+// if (number >= 100)
+// {
+//     //Уничтожаем хвостик после третьей цифры
+//     while (number > 999)
+//     {
+//         //Уничтожение последней цифры
+//         number = number / 10;
+//     }
+//     Console.WriteLine("третье число: ");
+//     //Выводим остаток от деления на 10( возвращаем последнюю цыфру)
+//     Console.WriteLine(number % 10);
+// }
+// else
+// {
+//     //Вывод ошибки
+//     Console.WriteLine("число не трёхзначное!");
+// }
+
+char[]digit = (Console.ReadLine()??"0").ToCharArray();
 
 
 if(digit.Length > 3)
 {
 Console.WriteLine(digit[2]);
-int num = int.Parse(Console.ReadLine()??"0");
+//int num = int.Parse(Console.ReadLine()??"0");
 }
 
 else
