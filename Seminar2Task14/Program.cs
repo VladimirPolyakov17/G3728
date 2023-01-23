57eb7f8d3e00abbf58978dd7ee57bc201978401f
Vladimir <vpolyakovup122@gmail.com>
2023-01-23 23:38:59 +0500

    Семинар 2. Задача 14

--- Seminar2Task14/Program.cs
+++ Seminar2Task14/Program.cs
@@ -7,17 +7,15 @@ int res1 = num1 % 7;
 int res2 = num1 % 23;
 if ((res1 == 0) && (res2 ==0 ))
 {
-    Console.Write("Число кратно 7 и 23"); 
+    Console.Write("Число кратно 7 и 23");
 }
 else
 {
     Console.Write("Число некратно 7 и 23");
 }
 int num = int.Parse(Console.ReadLine() ?? "0");
-if ((num%7==0)&&(num%23==0))
-{
-     Console.WriteLine("кратное");
-}else
-{
-     Console.WriteLine("не кратное 7и и 23ём");
+if ((num%7==0)&&(num%23==0)){
+    Console.WriteLine("кратное");
+}else{
+    Console.WriteLine("не кратное 7и и 23ём");
 }
