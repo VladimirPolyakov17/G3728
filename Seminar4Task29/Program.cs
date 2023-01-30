893e90efd08369337e1faaf1583ec21f45ed0ceb
Vladimir <vpolyakovup122@gmail.com>
2023-01-31 02:29:33 +0500

    Семинар 4. Задача 29. Домашнее задание

---
+++ Seminar4Task29/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res, int[] arr)
+{
+    Console.WriteLine(res);
+    PrintArr(arr);
+}
+
+int[] GenArr(int num)
+{
+    Random rnd = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 9);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+int arrLen = ReadData("Введите длину массива:");
+
+int[] arr = GenArr(arrLen);
+
+PrintData("Полученный массив:",arr);
