8b5fa879a77faf595a7cf89121269a3afa307a1f
Vladimir <vpolyakovup122@gmail.com>
2023-02-10 02:05:54 +0500

    Семинар 7. Задача 47. Домашнее задание

---
+++ Seminar7Task47/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+// m = 3, n = 4. 
+// 0,5 7 -2 -0,2 
+// 1 -3,3 8 -9,9 
+// 8 7,8 -7,1 9 
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+//1) Получение данных от пользователя
+int n = InputNum("Количество строк матрицы: ");
+int m = InputNum("Количество строк матрицы: ");
+
+//2) Генерация двумерного массива
+double[,] matix = FillTwoDimArray(n,m);
+
+//3) Печатаем двумерный массив
+PrintTwoDimArray(matix);
+
+
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод для заполнения двумерного массива
+double[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    double [,] outArray = new double[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.NextDouble();
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+
+//метод для печати двумерного массива
+void PrintTwoDimArray(double[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = (ConsoleColor)(new System.Random().Next(0,16));
+            //col[new System.Random().Next(0,16)]; 
+            Console.Write(inputArray[i, j] + " ");
+            Console.ResetColor(); 
+            j++;
+        }
+        //Console.Write("\n");
+        Console.WriteLine();
+        i++;
+    }
+}
