ebbd8257562aa1ef299d4864a450c93e0343056f
Vladimir <vpolyakovup122@gmail.com>
2023-02-14 00:23:07 +0500

    Семинар 8. Задача 56. Домашнее задание

---
+++ Seminar8Task56/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Генерируем двумерный массив, заполненный случайными числами
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+//Метод печати двумерного цветного массива
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i, j] % 15];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод нахождения индекса строки с минимальной суммой элементов
+int MinRow(int[,] matrix)
+{
+    int minIndex = 0; int minRow = int.MaxValue;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        if (minRow > sum)
+        {
+            minRow = sum; minIndex = i;
+        }
+    }
+    return minIndex;
+    
+}
+
+int row = ReadData("Введите количество строк "); // Пользователь вводит количество строк
+int column = ReadData("Введите количество столбцов "); // Пользователь вводит количество столбцов
+int[,] arr2D = Fill2DArray(row, column, 0, 9); //Заполнение массива  
+Print2DArrayColor(arr2D); //Печать двумерного цветного массива   
+int minIndex = MinRow(arr2D); //Нахождение индеса строки с минимальной суммой элементов
+Console.WriteLine("Индекс строки с минимальной суммой элементов: " + minIndex);
