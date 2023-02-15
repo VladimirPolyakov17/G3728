ff454f28abdeda408078f8733fc8d7ae7c17cdc7
Vladimir <vpolyakovup122@gmail.com>
2023-02-15 22:56:53 +0500

    Семинар 9. Задача 66. Домашнее задание

---
+++ Seminar9Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int sumMN)
+{
+    Console.WriteLine(sumMN);
+}
+
+//Метеод для нахождения суммы натуральных элементов в промежутке от M до N
+int RecSumMN(int M, int N)
+{
+    if (M >= N)
+    {
+        return N;
+    }
+    else
+    {
+        return M + RecSumMN(M + 1, N);
+    }
+}
+int M = ReadData("Введите число M: "); // Чтение данных из консоли
+int N = ReadData("Введите число N: "); // Чтение данных из консоли
+int sumMN = RecSumMN(M, N); //Расчет суммы с помощью рекурсии
+PrintResult(sumMN); //Печать результата
